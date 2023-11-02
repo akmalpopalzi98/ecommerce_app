70db5614ddd26e882c550532827e26fbe299e3a8
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-11-02 23:58:28 +0000

    chore: adding basket icon

--- src/components/Basket.tsx
+++ src/components/Basket.tsx
@@ -1,10 +1,41 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import { useContext } from "react";
+import { BasketContext } from "../context/BasketContext";
 
-const Basket = () => {
+const Basket = ({
+  onClick,
+  active,
+}: {
+  onClick: (bool: boolean) => void;
+  active: boolean;
+}) => {
+  const { basketContent } = useContext(BasketContext);
+
   return (
-    <Box>
-      <ShoppingBasketIcon />
+    <Box
+      sx={{
+        position: "absolute",
+        top: "0",
+        right: "0",
+        fontFamily: "monospace",
+        m: "19px",
+      }}
+    >
+      <Button
+        sx={{
+          color: "white",
+          "&:hover": {
+            color: "grey",
+          },
+        }}
+        onClick={() => {
+          onClick(!active);
+        }}
+      >
+        <ShoppingBasketIcon />
+        {basketContent.length}
+      </Button>
     </Box>
   );
 };
