474ab8d99f936fa23c12cbc542800bf2cfbcdd2b
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-11-02 15:54:12 +0000

    chore: adding basket page and component

---
+++ src/components/Basket.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+
+const Basket = () => {
+  return (
+    <Box>
+      <ShoppingBasketIcon />
+    </Box>
+  );
+};
+
+export default Basket;
---
+++ src/pages/BasketPage.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+
+const BasketPage = () => {
+  return <Box>Basket Page</Box>;
+};
+
+export default BasketPage;
