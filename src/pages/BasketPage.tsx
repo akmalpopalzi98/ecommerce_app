d3463f7a873fbd725bcc6ca5db77bead1eeb45b7
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-11-02 23:59:34 +0000

    chore: initial basket page

--- src/pages/BasketPage.tsx
+++ src/pages/BasketPage.tsx
@@ -1,7 +1,23 @@
 import { Box } from "@mui/material";
+import { useContext } from "react";
+import { BasketContext } from "../context/BasketContext";
 
 const BasketPage = () => {
-  return <Box>Basket Page</Box>;
+  const { basketContent } = useContext(BasketContext);
+  return (
+    <Box
+      sx={{
+        position: "absolute",
+        top: "100px",
+        right: "20px",
+        width: "300px",
+        height: "500px",
+        backgroundColor: "green",
+      }}
+    >
+      Basket Page
+    </Box>
+  );
 };
 
 export default BasketPage;
