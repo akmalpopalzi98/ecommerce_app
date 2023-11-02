3d92ec29de682d4a09785d3689a5691157fc56c7
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-11-02 23:58:00 +0000

    chore: adding more components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import { Box, Typography } from "@mui/material";
 import MainPage from "./pages/MainPage";
+import Basket from "./components/Basket";
+import BasketPage from "./pages/BasketPage";
+import { useState } from "react";
 
 function App() {
+  const [active, setActive] = useState(false);
   return (
     <Box>
-      <Typography
-        variant="h2"
-        sx={{ fontFamily: "pinyon script", textAlign: "left" }}
-      >
-        E-Shop
-      </Typography>
+      <Box sx={{ backgroundColor: "black" }}>
+        <Typography
+          variant="h2"
+          sx={{
+            fontFamily: "pinyon script",
+            textAlign: "left",
+            color: "white",
+            paddingLeft: "10px",
+          }}
+        >
+          E-Shop
+        </Typography>
+      </Box>
       <MainPage />
+      <Basket onClick={setActive} active={active} />
+      {active && <BasketPage />}
     </Box>
   );
 }
