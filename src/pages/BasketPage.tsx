1c40e4ddd0e9059301924c9f850f4693f5d00693
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-11-03 23:54:06 +0000

    chore: updating basket page

--- src/pages/BasketPage.tsx
+++ src/pages/BasketPage.tsx
@@ -1,28 +1,48 @@
-import { Box } from "@mui/material";
-import { useContext } from "react";
+import { Box, Typography } from "@mui/material";
+import { useContext, useState } from "react";
 import { BasketContext } from "../context/BasketContext";
+import BasketItem from "../components/BasketItem";
+import { ItemProps } from "../components/Item";
 
 const BasketPage = () => {
   const { basketContent } = useContext(BasketContext);
+  const [basketList, setBasketList] = useState(basketContent);
+
+  const handleAddItem = (item: ItemProps) => {
+    const updatedList = [...basketList];
+    updatedList.push(item);
+    setBasketList(updatedList);
+  };
+
   const renderedItems = basketContent.map((item) => {
     return (
-      <p key={item.id}>
-        {item.title} - {item.price}
-      </p>
+      <Box key={item.id}>
+        <BasketItem
+          product={item}
+          handleAdd={handleAddItem}
+          list={basketList}
+        />
+      </Box>
     );
   });
   return (
     <Box
       sx={{
         position: "absolute",
-        top: "100px",
-        right: "20px",
-        width: "300px",
+        top: "80px",
+        right: "0px",
+        width: "500px",
         height: "500px",
-        backgroundColor: "black",
+        backgroundColor: " #1c2833 ",
         color: "white",
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
       }}
     >
+      <Typography sx={{ textAlign: "center", marginTop: "10px" }}>
+        Cart
+      </Typography>
       {renderedItems}
     </Box>
   );
