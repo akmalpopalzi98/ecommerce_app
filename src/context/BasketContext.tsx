5be8de5cb545811692f32b5fa6c12267328247a5
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-11-04 00:34:58 +0000

    chore: adding new state

--- src/context/BasketContext.tsx
+++ src/context/BasketContext.tsx
@@ -12,11 +12,15 @@ import { ItemProps } from "../components/Item";
 type BasketContextType = {
   basketContent: ItemProps[];
   setBasketContent: Dispatch<SetStateAction<ItemProps[]>>;
+  itemQuantity: ItemProps[];
+  setItemQuantity: Dispatch<SetStateAction<ItemProps[]>>;
 };
 
 const initialContextValue: BasketContextType = {
   basketContent: [], // initial text value
   setBasketContent: () => {}, // a dummy function to avoid errors
+  itemQuantity: [], // initial text value
+  setItemQuantity: () => {}, // a dummy function to avoid errors
 };
 
 // Create a context with initial values
@@ -25,10 +29,13 @@ const BasketContext = createContext<BasketContextType>(initialContextValue);
 // Create a context provider component
 const BasketProvider = ({ children }: { children: ReactNode }) => {
   const [basketContent, setBasketContent] = useState<ItemProps[]>([]);
+  const [itemQuantity, setItemQuantity] = useState<ItemProps[]>(basketContent);
 
   const contextValue: BasketContextType = {
     basketContent,
     setBasketContent,
+    itemQuantity,
+    setItemQuantity,
   };
 
   return (
